8760d6d2cf81df37afbdf37404857894d6fd68ec
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-01 09:37:22 -0700

    create home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AngularCasts::Application.routes.draw do
+  get "home/index"
   scope :api do
     get "/screencasts(.:format)" => "screencasts#index"
     get "screencasts/:id" => "screencasts#show"
